f72fd4a8e91b85333f8d1fc7f25545463c3a1743
Dominique <dc@webformation.fr>
2026-01-07 11:55:39 +0100

    Version initiale - exo1 à 3

---
+++ exercice_2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace exercice_2
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+            message.Content = "Bonjour "  + nom.Text + " !";
+        }
+
+        private void nom_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            message.Content = "";
+        }
+    }
+}
