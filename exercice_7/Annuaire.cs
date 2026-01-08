c3c542ed99027d48bfc050a11a765e78d35e4a13
Dominique <dc@webformation.fr>
2026-01-08 17:08:51 +0100

    ajout exo 7 et 8

---
+++ exercice_7/Annuaire.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice_7
+{
+    public class Annuaire
+    {
+        public List<Personne> personnes = new List<Personne>();
+         void AjouterPersonne(Personne p)
+        {
+            personnes.Add(p);
+        }
+        public bool ContientPersonne(Personne p)
+        {
+            return personnes.Contains(p);
+        }
+    }
+}
