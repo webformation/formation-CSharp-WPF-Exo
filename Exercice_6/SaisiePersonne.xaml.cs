289a6dbf7a25176e6e56e1d1b604ed884aebd273
Dominique <dc@webformation.fr>
2026-01-08 16:08:22 +0100

    Ajout exo 5 et 6

---
+++ Exercice_6/SaisiePersonne.xaml.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace exercice_6
+{
+    /// <summary>
+    /// Logique d'interaction pour SaisiePersonne.xaml
+    /// </summary>
+    public partial class SaisiePersonne : Window
+    {
+        public Annuaire annuaire = new Annuaire();
+        Personne p = new Personne(String.Empty, DateTime.Now.Date);
+        public SaisiePersonne()
+        {
+            InitializeComponent();
+            Afficher.Visibility = Visibility.Hidden;
+            this.DataContext = p;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (string.IsNullOrWhiteSpace(nom.Text) || nom.Text.Length < 2)
+            {
+                MessageBox.Show("Nom invalide");
+                return;
+            }
+
+            else
+            {
+                //p.Nom = nom.Text;
+                //p.date_naissance = dateNaissance.SelectedDate.HasValue ? dateNaissance.SelectedDate.Value : DateTime.Now.Date;
+                Personne nouveau = new Personne(p.Nom, p.date_naissance);
+                if (annuaire.personnes.Contains(nouveau))
+                {
+                    MessageBox.Show($"Je vous ai déjà dit bonjour {nom.Text} !");
+                    return;
+                }
+                else
+                {
+                    annuaire.personnes.Add(nouveau);
+                    Afficher.Visibility = Visibility.Visible;
+
+                    MessageBox.Show($"Bonjour {nouveau.Nom}\nVous êtes né le {nouveau.date_naissance.ToShortDateString()}",
+                        "Informations Personne", MessageBoxButton.OK, MessageBoxImage.Information);
+                }
+            }
+
+
+        }
+
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+            if (annuaire.personnes.Count == 0)
+            {
+                MessageBox.Show("Aucune personne enregistrée.", "Annuaire", MessageBoxButton.OK, MessageBoxImage.Information);
+                return;
+            }
+            String liste = "Liste des personnes enregistrées :\n";
+            foreach (Personne p in annuaire.personnes)
+            {
+                liste += $"- {p.ToString()}\n";
+            }
+            MessageBox.Show(liste, "Annuaire", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
